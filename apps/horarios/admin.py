b28a3908c9cdbde71f0652641c885db0f3da4ed3
GUILDER <pplg39395@gmail.com>
2025-12-29 08:43:19 -0400

    Se muestra los turnos en el admin

--- apps/horarios/admin.py
+++ apps/horarios/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import DiasAtencion
+from .models import DiasAtencion, Turnos
 
 admin.site.register(DiasAtencion)
+
+@admin.register(Turnos)
+class TurnosAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'hora_ini', 'hora_fin', 'estado')
+    list_filter = ('estado',)
+    search_fields = ('nombre',)
